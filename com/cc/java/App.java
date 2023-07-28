0296f7a052faea7738d4641cb004dd8ef0d43c5a
Rekxis <rekxis@gmx.de>
2023-07-28 14:48:18 +0200

    first draft. todo: create HashMap containing Mitarbeiter attributes

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,12 +3,12 @@ package com.cc.java;
 public class App {
 
 	public static void main(String[] args) {
-	
-	
+
+		Mitarbeiter m001 = new Mitarbeiter("Schmidt", "Peter", "Dozent", 2015);
+
+		output(m001.getInfo());
 
 	}
-
-	
 
 	private static void output(String outStr) {
 		System.out.println(outStr);
